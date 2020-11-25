44f83927fad97f8b19e062aab17d01071d6c48bb
sitesoft1 <sitesoft1@gmail.com>
2020-11-25 06:19:29 +0200

    Подровнял контент

--- wp-content/themes/templvm/page.php
+++ wp-content/themes/templvm/page.php
@@ -51,67 +51,15 @@ get_header(); // подключаем header.php ?>
 <div id="main" class="wrapper style1">
 	<div class="container">
 <!-- temp END-->
-
-        <style>
-
-            .my_wrapper {
-                width: 100%;
-                display: grid;
-                grid-gap: 20px;
-                grid-template-columns: 1fr 5fr;
-                grid-template-rows: 1fr;
-
-                grid-template-areas:
-                        "s c";
-            }
-
-            @media screen and (max-width: 800px) {
-                .my_wrapper {
-                    width: 100%;
-                    display: grid;
-                    grid-gap: 5px;
-                    grid-template-columns: 1fr;
-                    grid-template-areas:
-                            "s"
-                            "c";
-                }
-            }
-            
-
-            .col1{
-                grid-area: s;
-                /* background: rgba(120, 10, 9, 0.965); */
-            }
-
-            .col2{
-                grid-area: c;
-                background: #00bb00;
-            }
-            
-            .left-sidebar-logo{
-                text-align: center;
-                margin-bottom: 15px;
-            }
-            
-            .left-sidebar-content{
-                background: rgba(120, 10, 9, 0.965);
-            }
-
-        </style>
-	
+  
 		<div class="my_wrapper row">
-   
             <div class="col1">
-                
                 <div class="left-sidebar-logo">
                     <img src="<?php the_post_thumbnail_url(); ?>">
                 </div>
                 <div class="left-sidebar-content">
-                    <h3>Левый сайдбар</h3>
-                    <p>Контент левого сайдбара</p>
                     <?php get_sidebar(); // подключаем sidebar.php ?>
                 </div>
-                
             </div>
             
             <div class="col2 <?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
@@ -126,9 +74,6 @@ get_header(); // подключаем header.php ?>
 				<?php } ?>
       
 						<section id="content">
-                        <?php if(the_title('','',false)){ ?>
-                            <h2><?php the_title(); ?></h2>
-                        <?php } ?>
 						<?php the_content(); ?>
 						</section>
 				<!-- temp END-->
