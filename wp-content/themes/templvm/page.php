ba86b47c3f72d0b815546b1fd962a25415a1a241
sitesoft1 <sitesoft1@gmail.com>
2020-11-25 06:05:26 +0200

    Добавил миниатюры на страницу

--- wp-content/themes/templvm/page.php
+++ wp-content/themes/templvm/page.php
@@ -12,7 +12,7 @@ get_header(); // подключаем header.php ?>
 <div id="colleges-header">
     <h1 class="text-h1-home">The first pvblic temple of roman tradition <br>bvilt since classic antiqvity</h1>
 </div>
-
+<!--
     <style>
         
         .my_wrapper {
@@ -45,13 +45,76 @@ get_header(); // подключаем header.php ?>
         <div class="item1">1</div>
         <div class="item2">2</div>
     </div>
+    -->
+
 
 <div id="main" class="wrapper style1">
 	<div class="container">
 <!-- temp END-->
+
+        <style>
+
+            .my_wrapper {
+                width: 100%;
+                display: grid;
+                grid-gap: 20px;
+                grid-template-columns: 1fr 5fr;
+                grid-template-rows: 1fr;
+
+                grid-template-areas:
+                        "s c";
+            }
+
+            @media screen and (max-width: 800px) {
+                .my_wrapper {
+                    width: 100%;
+                    display: grid;
+                    grid-gap: 5px;
+                    grid-template-columns: 1fr;
+                    grid-template-areas:
+                            "s"
+                            "c";
+                }
+            }
+            
+
+            .col1{
+                grid-area: s;
+                /* background: rgba(120, 10, 9, 0.965); */
+            }
+
+            .col2{
+                grid-area: c;
+                background: #00bb00;
+            }
+            
+            .left-sidebar-logo{
+                text-align: center;
+                margin-bottom: 15px;
+            }
+            
+            .left-sidebar-content{
+                background: rgba(120, 10, 9, 0.965);
+            }
+
+        </style>
 	
-		<div class="row">
-			<div class="<?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
+		<div class="my_wrapper row">
+   
+            <div class="col1">
+                
+                <div class="left-sidebar-logo">
+                    <img src="<?php the_post_thumbnail_url(); ?>">
+                </div>
+                <div class="left-sidebar-content">
+                    <h3>Левый сайдбар</h3>
+                    <p>Контент левого сайдбара</p>
+                    <?php get_sidebar(); // подключаем sidebar.php ?>
+                </div>
+                
+            </div>
+            
+            <div class="col2 <?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
 				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); // старт цикла ?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> <?php // контэйнер с классами и id ?>
 					
@@ -61,8 +124,7 @@ get_header(); // подключаем header.php ?>
 						<h2><?php the_title(); ?></h2>
 						</header>
 				<?php } ?>
-						
-						
+      
 						<section id="content">
                         <?php if(the_title('','',false)){ ?>
                             <h2><?php the_title(); ?></h2>
@@ -74,7 +136,6 @@ get_header(); // подключаем header.php ?>
 					</article>
 				<?php endwhile; // конец цикла ?>
 			</div>
-			<?php get_sidebar(); // подключаем sidebar.php ?>
 		</div>
 		
 <!-- temp -->
